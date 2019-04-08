993ba12ce47ff51bdd60cae74bc93dd9e95c9af2
rafiqin <rafiqin.r@hotmail.com>
2019-04-08 20:45:24 +1000

    second Batch

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
-import pkg from './package'
+const pkg = require('./package')
 
-export default {
+module.exports = {
   mode: 'universal',
 
   /*
@@ -9,17 +9,34 @@ export default {
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: pkg.description
+      }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      }
+    ]
   },
 
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: {
+    color: '#fff'
+  },
 
   /*
    ** Global CSS
